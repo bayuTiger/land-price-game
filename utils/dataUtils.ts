a9c12994e83f87413b7125edd3db51a4282c1648
vivi-tora <toranosuke.takayama@eisys.co.jp>
2024-07-04 20:38:33 +0900

    featureの型定義

--- utils/dataUtils.ts
+++ utils/dataUtils.ts
@@ -1,11 +1,18 @@
 // utils/dataUtils.ts
 import landPriceData from "../data/L02-23.geojson";
 
+interface Feature {
+  properties: {
+    L02_022: string;
+    // 他のプロパティもここに追加できます
+  };
+}
+
 export const getLandPriceData = (prefecture: string) => {
   return landPriceData.features
-    .filter((feature) => {
+    .filter((feature: Feature) => {
       const fullPrefecture = feature.properties.L02_022.split("　")[0];
       return fullPrefecture === prefecture;
     })
-    .map((feature) => feature.properties);
+    .map((feature: Feature) => feature.properties);
 };
